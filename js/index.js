6dc996cbc2241fd4dbcfeb09c971b317e38e4994
KhrisstianM <124510396+KhrisstianM@users.noreply.github.com>
2023-02-25 11:09:44 +0000

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -14,10 +14,10 @@ function play1() {
     document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
     if (randomNumber1 > randomNumber2) {
-        document.querySelector("h1").innerHTML = "Player One Wins!";
+        document.querySelector("h1").innerHTML = "Player 1 Wins!";
     }
     else if (randomNumber2 > randomNumber1) {
-        document.querySelector("h1").innerHTML = "Player Two Wins!";
+        document.querySelector("h1").innerHTML = "Player 2 Wins!";
     }
     else {
         document.querySelector("h1").innerHTML = "Draw";
